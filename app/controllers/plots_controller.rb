4078fe4e4507c9f5620478e6ddbe16b606ab54cf
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-06 10:37:51 -0600

    Add remove_plant action

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,5 +1,11 @@
 class PlotsController < ApplicationController
   def index
-    
+  end
+
+  def remove_plant
+    @plot = Plot.find(params[:id])
+    @plant = Plant.find(params[:plant_id])
+    PlotPlant.locate(@plot.id, @plant.id).delete
+    redirect_to plots_path
   end
 end
