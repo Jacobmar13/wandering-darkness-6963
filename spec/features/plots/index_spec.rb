d353966c579c6e4a1666b83f4085a154af4803cd
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-06 09:29:15 -0600

    Made test for index page content

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -4,8 +4,14 @@ RSpec.describe 'the plots index' do
   describe 'views' do
     it 'shows all the plots with their plants' do
       visit plots_path
-
-      expect(page).to have_content('')
+      save_and_open_page
+      expect(page).to have_content('Raspberry')
+      expect(page).to have_content('Strawberry')
+      expect(page).to have_content('Pineapple')
+      expect(page).to have_content("Plot Number: #{@plot1.number}")
+      expect(page).to have_content("Plot Number: #{@plot2.number}")
+      expect(page).to have_content("Plot Number: #{@plot3.number}")
+      expect(page).to have_content("Plot Number: #{@plot4.number}")
     end
   end
 
