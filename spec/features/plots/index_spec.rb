96b9625f16878d2c57a13792a0d872bb231fbb41
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-06 10:39:35 -0600

    Add tests for second user story

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -4,20 +4,43 @@ RSpec.describe 'the plots index' do
   describe 'views' do
     it 'shows all the plots with their plants' do
       visit plots_path
-      save_and_open_page
-      expect(page).to have_content('Raspberry')
-      expect(page).to have_content('Strawberry')
-      expect(page).to have_content('Pineapple')
+
+      expect(page).to have_content(@plant4.name)
+      expect(page).to have_content(@plant5.name)
+      expect(page).to have_content(@plant6.name)
       expect(page).to have_content("Plot Number: #{@plot1.number}")
       expect(page).to have_content("Plot Number: #{@plot2.number}")
       expect(page).to have_content("Plot Number: #{@plot3.number}")
       expect(page).to have_content("Plot Number: #{@plot4.number}")
     end
+    it "has button 'remove plant' from plot" do
+      visit plots_path
+
+      within("#plot#{@plot1.id}") do
+        expect(page).to have_button("Remove #{@plant4.name}")
+      end
+    end
   end
 
-  describe 'hyperlinks' do
-    it '' do
+  describe 'hyperlinks/buttons' do
+    describe 'Remove Plant' do
+      it 'can remove plant from plot, but not delete plant from other plots' do
+        visit plots_path
 
+        within("#plot#{@plot1.id}") do
+          expect(page).to have_content(@plant4.name)
+
+          click_button("Remove #{@plant4.name}")
+
+          expect(current_path).to eq(plots_path)
+
+          expect(page).to_not have_content(@plant4.name)
+        end
+
+        within("#plot#{@plot2.id}") do
+          expect(page).to have_content(@plant4.name)
+        end
+      end
     end
   end
 end
