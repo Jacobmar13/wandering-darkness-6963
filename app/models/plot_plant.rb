64244d95762b381ce2028dc9d2349e048854d528
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-06 10:38:08 -0600

    Add plotplant locate method

--- app/models/plot_plant.rb
+++ app/models/plot_plant.rb
@@ -1,4 +1,8 @@
 class PlotPlant < ApplicationRecord
   belongs_to :plot
   belongs_to :plant
+
+  def self.locate(plot_id, plant_id)
+    where('plot_id = ?', plot_id).where('plant_id = ?', plant_id).first
+  end
 end
