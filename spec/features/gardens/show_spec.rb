2bc29a592078c72a293cb1f0225232ad704e17fc
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-07 13:24:46 -0600

    Remove redundant locate method

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'the garden show' do
   describe 'views' do
     it 'shows the plants associated with the gardens plots' do
       visit garden_path(@garden1.id)
-      save_and_open_page
     end
   end
 end
