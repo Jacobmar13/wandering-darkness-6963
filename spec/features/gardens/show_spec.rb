733adf5e5a98fdd7f594a15bd5b9f6471c8d1d28
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-07 13:55:45 -0600

    Test views page for expected content

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe 'the garden show' do
   describe 'views' do
     it 'shows the plants associated with the gardens plots' do
       visit garden_path(@garden1.id)
+
+      expect(page).to have_content(@plant1.name)
+      expect(page).to have_content(@plant9.name)
+      expect(page).to_not have_content(@plant8.name)
     end
   end
 end
